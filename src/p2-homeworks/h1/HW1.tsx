2ed91d880e40bf51846ff31f9a7d944544c30d35
MariannaPo <mapodmasko@gmail.com>
2022-09-14 15:58:53 +0300

    other

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Message from "./Message";
+
 export const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
     message: 'some text',
-    // time: '22:00',
-    // name: 'Some Name',
+    time: '22:00',
+    name: 'Some Name',
 }
-
 
 
 function HW1() {
@@ -14,9 +14,8 @@ function HW1() {
     return (
         <div>
             <hr/>
-            <Message avatar={messageData.avatar} message={messageData.message}
-                // name={messageData.name}
-                // time={messageData.time}
+            <Message avatar={messageData.avatar} name={messageData.name} message={messageData.message} time={messageData.time}
+
             />
             <hr/>
         </div>
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,72 @@
 import React from 'react'
+import "./HW1";
+import {messageData} from "./HW1";
 
-function Message() {
+type MessageTypeProps = {
+    avatar: string,
+    message: string,
+    time: string,
+    name: string,
+    typer: Array<profilePicturePropsType>
+}
+
+function Message(props: MessageTypeProps) {
+    return (
+        <>
+            <ProfilePicture avatar={props.avatar}/>
+            <MessageWindow message={props.message}/>
+            <DataTime time={props.time}/>
+            <UserName name={props.name}/>
+
+        </>
+    )
+}
+
+type profilePicturePropsType = {
+    avatar: typeof messageData["avatar"],
+    message: typeof messageData['message'],
+    time: typeof messageData['time'],
+    name: typeof messageData['name'],
+}
+
+function ProfilePicture(props: profilePicturePropsType) {
+    const avaStyle = {
+        width: '100px',
+        height: '100px',
+        border: '2px solid black',
+        borderRadius: '50%',
+        display: 'inline-block',
+        padding: '2px',
+    }
     return (
         <div>
-
+            <img src={props.avatar} style={avaStyle} alt={'profile'}/>
         </div>
     )
 }
 
-export default Message
+function MessageWindow(props: profilePicturePropsType) {
+
+    return (
+        <pre>{props.message}</pre>
+    )
+}
+
+function DataTime(props: profilePicturePropsType) {
+
+    return (
+        <div>
+            <pre>{props.time}</pre>
+        </div>
+    )
+}
+
+function UserName(props: profilePicturePropsType) {
+    return (
+        <div>
+            <pre>{props.name}</pre>
+        </div>
+    )
+}
+
+export default Message;
